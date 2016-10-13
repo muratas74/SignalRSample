e1310455f53ba62720393e29f983e9c7c7563e39
Victor Tomaili <i@vctor.net>
2016-10-13 16:36:19 +0300

    server time

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -1,6 +1,8 @@
 ﻿
 using System;
 using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -8,11 +10,32 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
+        public Timer Timer { get; set; }
+
+        private void ServerClock(object state)
+        {
+            Clients.All.ServerClock(DateTime.UtcNow.ToString("O"));
+        }
+
+        public override Task OnConnected()
+        {
+            StartTime();
+            return base.OnConnected();
+        }
+
+        public void StartTime()
+        {
+            if (Timer != null)
+                return;
+
+            Timer = new Timer(ServerClock, this, 0, 1000);
+        }
+
         //server method
         public void Send(string username, string message)
         {
             //js method
-            Clients.All.Receive(HttpUtility.HtmlEncode(username), HttpUtility.HtmlEncode(message), DateTime.UtcNow.ToString("yyyyMMddhhmmss"));
+            Clients.All.Receive(HttpUtility.HtmlEncode(username), HttpUtility.HtmlEncode(message), DateTime.UtcNow.ToString("O"));
         }
     }
 }
